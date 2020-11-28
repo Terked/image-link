534232923ca1c03b28387a29ca042d7eadd54d7a
Stefan Filff <stefan@delightex.com>
2020-11-28 20:53:55 +0100

    Add show-journal

---
+++ show-journal.js
@@ -0,0 +1,9 @@
+//please make sure to set this script macro name to "show-journal"
+const entry = game.journal.get(args[0])
+const mode = args[1]
+
+if (entry) {
+    entry.show(mode, true)
+} else {
+    ui.notifications.notify(`Couldn't find journal id ${args[0]}`)
+}
