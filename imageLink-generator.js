90b91a18c9be2f6418c9c6a21b42c8ca1fc96197
Stefan Filff <stefan@delightex.com>
2020-11-28 22:16:50 +0100

    Update imageLink generator macro

--- imageLink-generator.js
+++ imageLink-generator.js
@@ -1,64 +1,57 @@
-/* Journal link builder v1.0 - variation from mestre-digital
-Source: https://raw.githubusercontent.com/brunocalado/mestre-digital/master/Foundry%20VTT/Macros/Ferramentas/Sound%20Link%20Builder.js
-*/
-
-main();
-
 function main() {
-    let journals = game.journal.entries.filter(journal => !!journal.data.img);
-    let journalOptions = "";
+    const journals = game.journal.entries.filter(journal => !!journal.data.img)
+    let journalOptions = ""
     for (let journal of journals) {
         journalOptions += `<option value=${journal.id}>${journal.data.name}</option>`
     }
 
-    let template = `  
+    const template = `  
   <p>    
-    Journal: <select id="journalID">${journalOptions}</select>
+    Journal entry <select id="journalDropdown">
+    ${journalOptions}</select>
   </p>
   <p>
-    Link name: <input id="linkName" type="text" style="width: 250px" value=''>
+    <textarea id="codeTextArea" rows="5" cols="33"></textarea>
   </p>
-  <br />
   `;
     new Dialog({
-        title: "Journal link builder v1.0",
+        title: "Generate image journal links",
+        buttons: {},
         content: template,
-        buttons: {
-            ok: {
-                label: "Generate",
-                callback: async (html) => {
-                    generateCode(html)
-                },
-            },
-        },
+        render: () => bindEvents(),
     }).render(true);
 }
 
-async function generateCode(html) {
-    const journalID = html.find("#journalID")[0].value;
-    const linkname = html.find("#linkName")[0].value;
-    const template = `<a class="journal_link" data-journalid="${journalID}" data-journaltype="${"image"}">${linkname}</a>`;
-
-    const form = `
-    <label>Copy this to the journal source code</label>
-    <textarea id="moduleTextArea" rows="5" cols="33">${template}</textarea>
-  `;
-
-    const dialog = new Dialog({
-        title: "module.json",
-        content: form,
-        buttons: {
-            use: {
-                label: "Copy"
-            }
-        },
-        render: html => {
-            const copyText = document.getElementById("moduleTextArea"); /* Get the text field */
-            copyText.select(); /* Select the text field */
-            document.execCommand("copy"); /* Copy the text inside the text field */
-            ui.notifications.notify(`Saved ${template} to clipboard`); /* Alert the copied text */
-            dialog.close()
-        }
-    })
-    dialog.close()
+function bindEvents() {
+    const journalDropdown = document.getElementById("journalDropdown")
+    journalDropdown.addEventListener("change", onDropdownChange)
 }
+
+function onDropdownChange() {
+    const journalDropdown = document.getElementById("journalDropdown")
+    const codeTextArea = document.getElementById("codeTextArea")
+
+    const selectedDropdownText = journalDropdown.options[journalDropdown.selectedIndex].text
+    const selectedDropdownId = journalDropdown.options[journalDropdown.selectedIndex].value
+    const generatedCode = `<a class="journal_link" data-journalid="${selectedDropdownId}" data-journaltype="${"image"}">${selectedDropdownText}</a>`
+
+    codeTextArea.value = generatedCode
+
+    copyCodeToClipBoard(codeTextArea)
+    clearSelection()
+    journalDropdown.focus()
+}
+
+function copyCodeToClipBoard(textArea) {
+    textArea.select()
+    document.execCommand("copy")
+    ui.notifications.notify(`Code copiedd to clipboard`)
+    
+}
+
+function clearSelection() {
+    window.getSelection()?.removeAllRanges()
+    document.selection?.empty()
+}
+
+main()
