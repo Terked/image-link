7de47d59632c7df296928bd8e3ce9ebc6075e23d
Terked <StefanFilff@gmail.com>
2020-11-29 16:26:37 +0100

    hide shown journal behind the triggering journal

--- macros/show-journal.js
+++ macros/show-journal.js
@@ -1,9 +1,23 @@
 //Please make sure to set this script macro name to "show-journal"
+const entryId = args[0]
 const entry = game.journal.get(args[0])
 const mode = args[1]
 
-if (entry) { 
-    entry.show(mode, true)
+if (entry) {
+    entry.show(mode, true).then(() => checkForPopup())
 } else {
-    ui.notifications.notify(`Couldn't find journal id ${args[0]}`)
+    ui.notifications.notify(`Couldn't find journal with id ${entryId}`)
+}
+
+function checkForPopup() {
+    const popup = document.getElementById(`journal-${entryId}`)
+    if (!popup) {
+        delay(50).then(() => checkForPopup())
+    } else {
+        popup.style.zIndex = 1
+    }
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
